3642ff2b6632c356f1f581e2d58b372495c30071
Eraldo <rangelfisica@hotmail.com>
2018-06-18 13:02:21 -0300

    update mackefile

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /Users/astrowar/git/CMatchIteractiveFic
+# Install script for directory: C:/Users/Eraldo.rangel/Source/Repos/CInform
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/usr/local")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/AppMatch")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "")
+    set(CMAKE_INSTALL_CONFIG_NAME "Release")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -35,5 +35,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/astrowar/git/CMatchIteractiveFic/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Eraldo.rangel/Source/Repos/CInform/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
