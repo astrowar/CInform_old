c1a6868042d515e413c1e291f98ef6b5f6328e6e
Eraldo <rangelfisica@hotmail.com>
2018-06-25 16:24:50 -0300

    add

---
+++ AppMatch/src/interpreter/CResultMatch.cpp
@@ -0,0 +1,25 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+#include "CResultMatch.hpp"
+
+using namespace CBlocking;
+ 
+
+CResultMatch::CResultMatch(string s, HBlock b) : hasMatch(true)
+{
+	maptch[s] = b;
+}
+
+CResultMatch::CResultMatch(bool r) :hasMatch(r)
+{
+
+}
+
+void CResultMatch::append(const CResultMatch & r)
+{
+	for(auto &kv: r.maptch)
+	{
+		maptch[kv.first] = kv.second;
+	}
+};
